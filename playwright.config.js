c0619e47672c93c91e27a3e4d9a31cbd922e476a
Hemateja <2489619@cognizant.com>
2026-05-21 10:02:18 +0530

    Fixed issue

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'], // keep console output
-    ['githiub']
+    ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
